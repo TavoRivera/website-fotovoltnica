4c192acbd0c58b759371e56ab02b8f89cf2700aa
Octavio Rivera <octaviostyles02@gmail.com>
2021-06-07 00:43:45 -0600

    :construction: -  @TavoRivera  - finished index and create models

--- fotovoltnica/inventario/models.py
+++ fotovoltnica/inventario/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+#servicios
+class Servicio(models.Model):
+    name = models.CharField(max_length=42, blank=False)
+    image = models.ImageField(upload_to="static/images/services/", blank=True)
+    comment = models.CharField(max_length=120, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+#Proyectos
+class Proyecto(models.Model):
+    name = models.CharField(max_length=42, blank=False)
+    place =  models.CharField(max_length=32, blank=False)
+    image = models.ImageField(upload_to="static/images/services/", blank=True)
+    comment = models.CharField(max_length=128, blank=True)  
+    
+    def __str__(self):
+        return f"{self.name} - {self.place}"
+
+class Capacidad(models.Model):
+    capacity = models.CharField(max_length=18, blank=True)
+
+    def __str__(self):
+        return f"{self.capacity}"
--- fotovoltnica/inventario/views.py
+++ fotovoltnica/inventario/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
 def index(request):
-    # verifica si el usuario está logueado
-    return render(request, "inventario/layout.html")
+    
+    context = {
+        'servicios': Servicio.objects.all(),
+        'proyectos': Proyecto.objects.all()
+    }
+    return render(request, "inventario/layout.html", context)
